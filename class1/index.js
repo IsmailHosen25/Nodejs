2357cd7e9f54e882b19b52d1bb575b4d6b7dc638
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-02-03 08:34:27 +0600

    class1 add

---
+++ class1/index.js
@@ -0,0 +1,5 @@
+const {hp}=require("./student");
+// const h=require("./student")
+console.log(hp);
+// console.log(h.sum()+" "+ h.hello());
+// console.log(h.sum()+h.hp);
---
+++ class1/student.js
@@ -0,0 +1,15 @@
+function hello(){
+    return"hasan"
+}
+const sum = () => {
+return(25)
+}
+const hp=5678;
+// exports.hello=hello;
+// exports.getsum=sum;
+// exports.results=hp;
+module.exports={
+    hello,
+    sum,
+    hp
+}
