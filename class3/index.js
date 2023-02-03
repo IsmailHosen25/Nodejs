c5a45f4ed5d8d9d7663bec1b036cefa2e86604b3
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2023-02-03 09:22:31 +0600

    class3 added

---
+++ class3/index.js
@@ -0,0 +1,15 @@
+// const os =require("os");
+// const {userInfo, homedir}=require("os")
+// console.log(os.userInfo());
+// console.log(os.homedir());
+// console.log(__dirname);
+// console.log(_filename);
+// console.log(os.hostname());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+const path =require("path")
+// const extName=path.extname("index.html")
+// console.log(extName);
+
+// const joinName=path.join(__dirname + "/views");
+// console.log(joinName)
